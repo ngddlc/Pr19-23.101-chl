e92a3dd836c93af671c56d3cd38e46d2e1250c5e
ngdd <leonidcernavskoj@gmail.com>
2026-07-03 22:00:02 +0700

    Downgrade compileSdk and targetSdk to 34 for offline compilation compatibility

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.pr_23101_v1"
-    compileSdk = 36
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.pr_23101_v1"
         minSdk = 26
-        targetSdk = 36
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
