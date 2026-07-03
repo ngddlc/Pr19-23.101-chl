b5abb52eec067297fa43a88334cb099bca8eedd2
ngdd <leonidcernavskoj@gmail.com>
2026-07-03 21:17:55 +0700

    Fix package name inside instrumented tests and update IDE name config

--- app/src/androidTest/java/com/example/pr_23101_v1/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/pr_23101_v1/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.example.pr19_23101_fi;
+package com.example.pr_23101_v1;
 
 import android.content.Context;
 
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.example.pr19_23101_fi", appContext.getPackageName());
+        assertEquals("com.example.pr_23101_v1", appContext.getPackageName());
     }
 }
